e2719af823afdac5609719ac4869a89179e2b26c
ifeeney <ifeeney@wpi.edu>
2020-10-02 12:59:33 -0400

    Update motors formatting

---
+++ Firmware/Motors/servoRosAttempt/Motors.h
@@ -0,0 +1,12 @@
+/**
+ * @file Motors.h
+ * @brief Subsystem for motor drivers
+ */
+#pragma once
+#include <stdint.h>
+
+namespace Motors
+{
+  void init();
+  void testMotion(int i);
+}
--- Firmware/Motors/servoRosAttempt/servoRosAttempt.ino
+++ Firmware/Motors/servoRosAttempt/servoRosAttempt.ino
@@ -1,78 +1,77 @@
 #include <ros.h>
 #include <std_msgs/Int16.h>
+#include <Servo.h>
+#include <DigitalOut.h>
 
 ros::NodeHandle nh;
 
 std_msgs::Int16 str_msg;
 ros::Publisher potAngles("potAngles", &str_msg);
 
+/**
+ * Private subsystem info
+ */
+namespace Motors
+{
+  //temp
+  const int sensorPin = 14;
+  DigitalOut enable_output(pin_en);
 
-#include <Servo.h>
-
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
-int sensorValue;
-int pos = 0;    // variable to store the servo position
-int sensorPin = A0;
-void setup() {
-  Serial.begin(9600);
-  pinMode(sensorPin, INPUT);
-  myservo.attach(2);  // attaches the servo on pin 9 to the servo object
-    nh.initNode();
-   nh.advertise(potAngles);
-   
-   
+  Servo myservo[6];  // create servo object to control a servo
+  
+  // Pin definitions
+  const uint8_t pin_en = 2;
+  const uint8_t servo_pins[6] = {2, 3, 4, 5, 6, 7};
+  
+  // Init flag
+  bool init_complete = false;
 }
 
-void loop() {
+/**
+ * @brief Initializes motor drivers
+ */
+void Motors::init()
+{
+  if (!init_complete)
+  {
+    // Disable motor drivers
+    enable_output = 0;
+
+    // Initalize Servo objects
+    for (uint8_t j = 0; j < num_joints; j++)
+    {
+        myservo[j].attach(servo_pins[j]);
+    }
+    
+    // Set init flag
+    init_complete = true;
+  }
+}
+
+void Motors::testMotion(int i) {
+  int pos = 0;
   for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    sensorValue = myservo.read();
-    str_msg.data = sensorValue;
-    potAngles.publish(&str_msg);
+    myservo[i].write(pos);              // tell servo to go to position in variable 'pos'
+    sensorValue = myservo[i].read();
+    //str_msg.data = sensorValue;
+    //potAngles.publish(&str_msg);
     //sensorValue = analogRead(sensorPin);
    // Serial.println(sensorValue);
     delay(250);                       // waits 15ms for the servo to reach the position
  
   }
   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    sensorValue = myservo.read();
+    myservo[i].write(pos);              // tell servo to go to position in variable 'pos'
+    sensorValue = myservo{i].read();
     //sensorValue = analogRead(sensorPin);
-    str_msg.data = sensorValue;
-    potAngles.publish(&str_msg);
+    //str_msg.data = sensorValue;
+    //potAngles.publish(&str_msg);
     //Serial.println(sensorValue);
     delay(250);                       // waits 15ms for the servo to reach the position
   }
 }
   
-
-
-
-//int sensorPin = A0;    // select the input pin for the potentiometer
-//int sensorValue;  // variable to store the value coming from the sensor
-//
-//
-//
-//void setup() {
-//
-//  //Serial.begin(9600);
-//  pinMode(sensorPin, INPUT);
-//  nh.initNode();
-//  nh.advertise(potAngles);
-//}
-//
-//void loop() {
-//  // read the value from the sensor:
-//  sensorValue = analogRead(sensorPin);
-//  str_msg.data = sensorValue;
-//  potAngles.publish(&str_msg);
-//  //Serial.println(String(sensorValue));
-//  delay(500);
-//  nh.spinOnce();
-//  
-//}
 
 
 
