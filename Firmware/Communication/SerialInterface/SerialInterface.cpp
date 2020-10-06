178ef355452a49dca743d2935e03a710cab2ff3d
Mario <macastro2@wpi.edu>
2020-10-06 18:57:33 -0400

    Added main

---
+++ Firmware/Communication/SerialInterface/SerialInterface.cpp
@@ -0,0 +1,43 @@
+
+#include "SerialInterface.h"
+
+
+#define SEND_EVERY_MS 1000
+
+
+namespace SerialInterface
+{
+  int sensorPin = 32;    // select the input pin for the potentiometer
+  int sensorValue;  // variable to store the value coming from the sensor
+  uint16_t toSend[9];
+}
+bool SerialInterface::init() {
+
+  pinMode(sensorPin, INPUT);
+  Serial.begin(9600);
+
+}
+void SerialInterface::update() {
+
+  //Set each of these equal to the correct values coming from the arm
+  toSend[0] = analogRead(sensorPin); // Servo One
+  toSend[1] = 10; //Servo Two
+  toSend[2] = 20; //Servo Three
+  toSend[3] = 30; //Encoder One
+  toSend[4] = 40; //Encoder Two
+  toSend[5] = 50; //Encoder Three
+  toSend[6] = 60; //Encoder Four
+  toSend[7] = 70; //Gripper Engaged
+  toSend[8] = 80; //Arm Locked
+
+
+  String servoCompressed = String(toSend[0]) + ',' + String(toSend[1]) + ',' + String(toSend[2]); //String of Servo Data
+  String encoderCompressed = String(toSend[3]) + ',' + String(toSend[4]) + ',' + String(toSend[5]) + ',' + String(toSend[6]); //String of Encoder Data
+  String otherData = String(toSend[7]) + ',' + String(toSend[8]); // String of other data
+   
+  Serial.println(servoCompressed + ',' + encoderCompressed + ',' + otherData); //Concats all strings together and sends over serial. 
+  //Make sure companion Python Script is running to parse and send to ROS
+
+  
+  delay(SEND_EVERY_MS);
+}
